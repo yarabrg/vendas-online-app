052f92a01926d77639ddcaca7f4766b0048bb283
Yara <yrbraga@alu.ufc.br>
2024-01-19 18:51:38 -0300

    feat: inserção do styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const Button = styled.Button`
+  height: 20;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text> TESTANDO</Text>
+      <Text style={styles.container}>Testando</Text>
+      <Button title="TÍTULO TESTE" />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    color: 'blue',
+  },
+});
 
 export default App;
